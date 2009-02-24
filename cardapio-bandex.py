3d70dc1ace76eca5b48047b680d10095a0b8daff
Raphael Kubo da Costa <kubito@gmail.com>
2009-02-24 01:42:14 -0300

    Make the parser even more error-proof by trying to parse the date in two ways.

--- cardapio-bandex.py
+++ cardapio-bandex.py
@@ -74,21 +74,40 @@ class MenuParser(object):
   def getSource(self):
     return urllib2.urlopen(MenuParser.SOURCE_URL).read()
 
+  def getTagText(self, tag):
+    """
+    Receives a Tag object and returns its stripped text.
+    """
+    return ''.join(tag.findAll(text=True)).replace(unichr(160), ' ')
+
   def parseMenu(self):
     soup = BeautifulSoup(self.sourceText,
                          markupMassage = MenuParser.SOURCE_FIXES,
                          convertEntities = BeautifulStoneSoup.HTML_ENTITIES)
 
-    menuBlock  = soup.findAll('p', style="text-align: left")
     menuKeys   = ('data', 'principal', 'sobremesa', 'salada', 'suco')
+
+    # Add the entries which have the same format ('key: value')
+    menuBlock  = soup.findAll('p', style="text-align: left")
     menuValues = []
 
     for line in menuBlock:
-      strLine = ''.join(line.findAll(text=True)).replace(unichr(160), ' ')
+      strLine = self.getTagText(line)
       match   = re.match(r'[\w\s]+:\s*(.+)', strLine)
 
       if match:
         menuValues.append(match.group(1))
+
+    # If there were only 4 entries in the usual format, probably date is missing
+    if len(menuValues) == 4:
+      # Add the date, whose format tends to vary.
+      # Finding the main meal and simply retrieving the previous sibling
+      # seems to be the most error-proof method.
+      mainMeal = soup.find(text = re.compile(r"prato\s+principal", re.IGNORECASE)).parent.parent
+      mealDate = mainMeal.findPreviousSibling('p')
+      menuValues = [self.getTagText(mealDate)] + menuValues
+    elif len(menuValues) != 4 and len(menuValues) != 5:
+      raise ValueError, "The menu in the site has an invalid format"
 
     return Menu(dict(zip(menuKeys, menuValues)))
 
