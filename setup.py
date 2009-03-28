1223ba19ab431298bb065920f8c51440b8928df1
Raphael Kubo da Costa <kubito@gmail.com>
2009-03-28 13:53:00 -0300

    Created setup.py for an easier installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='CardapioBandex',
+      version='0.1.0',
+      description='State University of Campinas\' university restaurant\'s menu parser.',
+      long_description='A library to parse State Univerisy of Campinas\' university restaurant\'s menu.',
+      author='Raphael Kubo da Costa',
+      author_email='kubito@gmail.com',
+      url='http://github.com/rakuco/cardapio-bandex',
+      license='BSD',
+      packages=['CardapioBandex']
+     )
